6a69395af49836ba24bc2101b645957e0a19f9e4
dbxkr97 <dbxkr97@kaist.ac.kr>
2018-11-07 22:11:15 +0900

    yutak merge
    # Conflicts:
    #	app/src/main/java/com/example/yutakkim/ex20181107/MainActivity.java

--- app/src/main/java/com/example/yutakkim/ex20181107/MainActivity.java
+++ app/src/main/java/com/example/yutakkim/ex20181107/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //New Branch
+        //re Main Branch
         //haha
         //hoho
         //hihi
